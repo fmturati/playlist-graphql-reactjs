8efd0f6c8f70c75abaabccf2e0bab5180c6f358f
Fhelipe Turati <fmturati@gmail.com>
2020-02-04 11:25:20 -0800

    Frontend integrated with our server, Add new books and view details components

--- server/app.js
+++ server/app.js
@@ -2,18 +2,25 @@ const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
+// set .env file
 require("dotenv").config();
 
 const app = express();
 
-const port = process.env.PORT || 5000;
+// allow cross-origin requests
+app.use(cors());
 
+const port = process.env.PORT || 4000;
+
+// connect to MongoDB
 mongoose.connect(process.env.DATABASE_URI, { useNewUrlParser: true });
 
 mongoose.connection.once("open", () => {
   console.log("MongoDB Connected!");
 });
+
 app.use(
   "/graphql",
   graphqlHTTP({
